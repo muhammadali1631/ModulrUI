5a8dce2ba41ba3b19595f63d9bdba4f8b9e75cea
Ali Shahzad <160423266+muhammadali1631@users.noreply.github.com>
2026-07-31 05:14:45 +0500

    Change server URL to production endpoint

--- modulrui-client/src/App.jsx
+++ modulrui-client/src/App.jsx
@@ -12,7 +12,7 @@ import axios from 'axios'
 import "./App.css";
 import FixedIcon from './components/FixedIcon'
 
-export const ServelUrl = "http://localhost:8000"
+export const ServelUrl = "https://modulr-ui.vercel.app"
 function App() {
   
   const dispatch = useDispatch()
